8ea8ccb60f80ff5a3e717e1bfeec05928a5d75a3
Keith Woods <wkeith@live.com>
2022-05-06 09:31:16 -0400

    project finish & no errors
    add data and project finished without errors

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
-var qwerty = document.getElementById('qwerty');
-var phrase1 = document.getElementById('phrase');
-var missed = 0;
+const qwerty = document.getElementById('qwerty'); 
+const phrase1 = document.getElementById('phrase');
+const startbutton = document.getElementById('overlay');
+let missed = 0;
 
-function resetbtn(){
-
-  document.getElementById('overlay').style.display="none";
-
+function resetbtn() {
+  startbutton.style.display = 'none';
 }
 
 const phrases = [
@@ -14,149 +13,98 @@ const phrases = [
   'see you soon',
   'on my way',
   'looking forward to seeing you'
-
 ];
-var randomphrase = phrases[Math.floor(Math.random()*phrases.length)];
-function getRandomPhraseAsArray(arr){
 
-console.log(randomphrase);
-var splitrandomphrase = randomphrase.split("");
-console.log(splitrandomphrase);
-return splitrandomphrase;
-
+function getRandomPhraseAsArray(arr) {
+  const randomphrase = phrases[Math.floor(Math.random() * arr.length)];
+  const splitrandomphrase = randomphrase.split("");
+  return splitrandomphrase;
 }
-getRandomPhraseAsArray(phrases);
-
 
 const newsplitphrase = getRandomPhraseAsArray(phrases);
-  addPhraseToDisplay(newsplitphrase);
+addPhraseToDisplay(newsplitphrase);
 
-function addPhraseToDisplay(newsplitphrase){
-    // do stuff any arr that is passed in, and add to `#phrase ul`
+console.log(newsplitphrase);
+function addPhraseToDisplay(newsplitphrase) {
+  // do stuff any arr that is passed in, and add to `#phrase ul`
+  newsplitphrase.forEach(function (character) {
+    const ul = document.getElementById("phrase");
+    const newli = document.createElement('li');
 
-  newsplitphrase.forEach(function(character){
-    var ul = document.getElementById("phrase");
-    var newli = document.createElement('li');
     newli.appendChild(document.createTextNode(character));
+    ul.appendChild(newli);
 
-      ul.appendChild(newli);
-    if (character != ' '){
-
+    if (character !== ' ') {
       newli.setAttribute('class', 'letter');
 
-
-    }
-    else if(character = ' ') {
+    } else if (character === ' ') {
       newli.setAttribute('class', 'space');
     }
-});
+  });
 }
 
+const letter = document.getElementsByClassName('letter');
+function checkLetter(selectedbutton) {
 
+  const letterarray = Array.from(letter);
 
+  let match = null;
 
+  //loops through one single time
+  for (let i = 0; i < letterarray.length; i++) {
+    const maskedphrase = letterarray[i];
+    if (maskedphrase.innerHTML === selectedbutton) {
+      maskedphrase.classList.add('show');
+      match = maskedphrase.innerHTML;
+      console.log(match);
+    }
+  } /*............................The loops end ........................................................*/
+  return match;
+} //end of checkletter function
 
 //Event Listener
-  var keyboard = document.getElementsByClassName('keyrow');
-  var missed = 0;
-  var bobo;
-
-  console.log(keyboard);
-  for (const keyrow of keyboard){
-    keyrow.addEventListener('click', event => {
-
-      if (event.target.tagName == 'BUTTON') {
-
-        console.log('Button clicked');
-        //add chosen class to keyboard
-        event.target.setAttribute('class', 'chosen');
-        //disable the button
-        //event.target.setAttribute('disabled', 'true');
-        console.log('the button '+ '"'+ event.target.innerHTML + '"' +' has been used. It will be disabled.');
-        //get the value of the button clicked
-        var selectedbutton = event.target.innerHTML;
-
-        console.log(selectedbutton);
-        //return selectedbutton;
-
-
-
-        //start of checkletter function
-          function checkLetter(selectedbutton) {
-
-          const letter = document.getElementsByClassName('letter');
-        //  console.log(letter);
-          const letterarray = Array.from(letter);
-
-
-  /*............................The loops begin ........................................................*/
-
-
-
-
-               //loops through one single time
-              for (var i=0, len= letterarray.length; i<len ; i++){
-                someFn(letterarray[i]);
-
-                function someFn(){
-                  var maskedphrase = letterarray[i];
-                  console.log(maskedphrase.innerHTML);
-                  if (maskedphrase.innerHTML===selectedbutton){
-                    console.log("There is a match");
-                    maskedphrase.setAttribute('class', 'show');
-                    console.log(maskedphrase.innerHTML);
-                    return maskedphrase.innerHTML;
-                  }
-                  else{
-                    console.log("No match found")
-                    return null;
-                  }
-                }
-                console.log(someFn(letterarray[i]));
-                return someFn(letterarray[i]);
-            }
-
-
-/*............................The loops end ........................................................*/
-
-
-        }
-        //end of checkletter function
-
-
-         // call the CheckLetter function
-        checkLetter(selectedbutton);
-
-         var letterfound = checkLetter();
-
-
-        console.log(letterfound);
-        console.log(letterfound);
-        console.log(letterfound);
-
-        //Check the value of letter found
-        if (letterfound === null){
-          missed += 1
-          console.log(missed);
-
-          /** change the scoreboard  **/
-          const scoreboard = document.getElementById("scoreboard");
-          //remove a blue heart
-          blueheart = document.getElementsByTagName('<li>')
-          scoreboard.innerHTML = '';
-          //scoreboard.parentNode.removeChild(scoreboard.firstChild);
-
-
-
-        }
-
-
-
+const keyboard = document.getElementsByClassName('keyrow');
+for (const keyrow of keyboard) {
+  keyrow.addEventListener('click', event => {
+    if (event.target.tagName == 'BUTTON') {
+      //add chosen class to keyboard
+      event.target.setAttribute('class', 'chosen');
+      //disable the button
+      event.target.setAttribute('disabled', 'true');
+      //get the value of the button clicked
+      const selectedbutton = event.target.innerHTML;
+      console.log(selectedbutton);
+      // call the CheckLetter function
+      const letterfound = checkLetter(selectedbutton);
+      //Check the value of letter found
+      if (letterfound === null) {
+        //remove a blue heart
+        const blueheart = document.querySelectorAll('.tries img');
+        blueheart[missed].src = 'images/lostHeart.png';
+        missed += 1;
       }
+    }
+    checkWin();
+  });
+}
 
-
-
-
-    });
-
+function checkWin() {
+  var correctLetter = document.getElementsByClassName('show');
+  var overlay = document.getElementById('overlay');
+  var overlaychildren = document.getElementById('overlay').children;
+  if (correctLetter.length == letter.length){
+  console.log('You Won');
+  overlay.style.display = 'block';
+    overlay.setAttribute('class', 'win');
+    overlaychildren[0].innerHTML = 'Congratulations You Won';
+    overlaychildren[1].innerHTML = 'Play Again';
   }
+  else if (missed >= 5) {
+    console.log("You Lost");
+    overlay.style.display = 'block';
+    overlay.setAttribute('class', 'lose');
+    overlaychildren[0].innerHTML = 'Sorry You Lost';
+    overlaychildren[1].innerHTML = 'Try Again';
+  }
+
+}
